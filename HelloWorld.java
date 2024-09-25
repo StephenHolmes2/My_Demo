0ed4fe1f5881d98aaa4e09cb4aecf563ec408598
StephenHolmes2 <79773751+StephenHolmes2@users.noreply.github.com>
2024-09-25 13:23:40 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/* 
+  Author: Stephen Holmes
+  FileName: HelloWorld_CDE.java
+ */
+package helloCDE;
+
+public class HelloWorld {
+    //Prints "Hello CDE World!"
+    public static void main(String[] args) {
+		
+		System.out.println("Hellow CDE World!");
+	}
+  
+}
