44fad27cb47d60d6059cce674688d3823cf67f13
StephenHolmes2 <79773751+StephenHolmes2@users.noreply.github.com>
2024-09-25 16:44:46 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,13 +2,13 @@
   Author: Stephen Holmes
   FileName: HelloWorld_CDE.java
  */
-package helloCDE;
+
 
 public class HelloWorld {
     //Prints "Hello CDE World!"
     public static void main(String[] args) {
 		
-		System.out.println("Hellow CDE World!");
+		System.out.println("Hello CDE World!");
 	}
   
 }
